e64f54ee020a6596f1b2d649ab2a290571da16b3
dezfuture <agrim2000@gmail.com>
2021-06-14 20:41:27 +0530

    Just imported firebase and started with the home component

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Home.module.css";
+
+function Home() {
+  return (
+    <div className={classes.home}>
+      <h1>This is the home component</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/plan.md
@@ -0,0 +1,14 @@
+<!-- here we plan what we gonna do -->
+
+here we have used material ui as the library and also a lot of 'BEM' stuff is used
+
+Header
+
+Banner
+{Search}
+
+Cards
+
+Footer
+
+{SearchPage}
