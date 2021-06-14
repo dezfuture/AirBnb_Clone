de723f849995760b1578d795a10aab5d76e0e407
dezfuture <agrim2000@gmail.com>
2021-06-14 21:32:01 +0530

    till here we worked on the header component and imported several icons from the material UI library and focused on the css of the header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "./Header";
 import Home from "./Home";
 
 function App() {
   return (
     // BEM
     <div className="app">
+      <Header />
       <Home />
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import classes from "./Header.module.css";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+
+function Header() {
+  return (
+    <div className={classes.header}>
+      <img
+        className={classes.header__icon}
+        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+        alt=""
+      />
+
+      <div className={classes.header__center}>
+        <input type="text" />
+        <SearchIcon />
+      </div>
+
+      <div className={classes.header__right}>
+        <p>Become a Host</p>
+        <LanguageIcon />
+        <ExpandMore />
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
